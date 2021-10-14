2db9a815d91964b00e5f1a33ff8b134660462bc8
dinesvlarsen <dinesvlarsen@gmail.com>
2021-10-14 13:57:51 +0200

    added all the product information in objects within an array

---
+++ js/app.js
@@ -0,0 +1,52 @@
+const products = [
+	{
+		name: 'Gateron Black Ink V2',
+		type: 'Linear',
+		price: '1.20',
+		img: 'gateron-black-ink-v2-linear-png',
+	},
+	{
+		name: 'Gateron Orange',
+		type: 'Tactile',
+		price: '1.40',
+		img: 'gateron-orange-tactile.png',
+	},
+	{
+		name: 'Gateron Pink',
+		type: 'Linear',
+		price: '2.10',
+		img: 'gateron-pink-linear.png',
+	},
+	{
+		name: 'Gateron Red Ink V2',
+		type: 'Linear',
+		price: '1.80',
+		img: 'gateron-red-ink-v2-linear.png',
+	},
+	{
+		name: 'JWICK Red',
+		type: 'Linear',
+		price: '2.40',
+		img: 'jwick-red-linear.png',
+	},
+	{
+		name: 'Tealios V2',
+		type: 'Linear',
+		price: '5.0',
+		img: 'tealios-v2-linear.png',
+	},
+	{
+		name: 'Zealios V2',
+		type: 'Tactile',
+		price: '2.40',
+		img: 'zealios-v2-linear.png',
+	},
+	{
+		name: 'Zilents',
+		type: 'Tactile',
+		price: '1.90',
+		img: 'zilents-tactile.png',
+	},
+];
+
+/* Maybe make factory function? */
